80ffafd949cf0a7b6d13426c435c51195eb5242f
lautaro valle lopez <lautarovallelopez@gmail.com>
2024-02-20 17:49:24 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Texto\nen\nlineas\nseparadas");
+        System.out.println("Esto no se ve\rCarro retornado");
+        System.out.println("Borro espacio \ba la izquierda");
+        System.out.println("Imprimo la barra invertida: \\");
+        System.out.println("Imprimo la comilla simple: \'");
+        System.out.println("Imprimo la comilla soble: \"");
+    }
+}
