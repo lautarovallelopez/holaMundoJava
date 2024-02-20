0fc2704967d81f898afd105ae889adbac65b62b4
lautaro valle lopez <lautarovallelopez@gmail.com>
2024-02-20 18:15:08 -0300

    create scanner object and read a value

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Texto\nen\nlineas\nseparadas");
         System.out.println("Esto no se ve\rCarro retornado");
         System.out.println("Borro espacio \ba la izquierda");
         System.out.println("Imprimo la barra invertida: \\");
-        System.out.println("Imprimo la comilla simple: \'");
+        System.out.println("Imprimo la comilla simple: '");
         System.out.println("Imprimo la comilla soble: \"");
+
+        System.out.print("Ingrese un valor numerico: ");
+        int numero = Integer.parseInt(scanner.nextLine());
+        System.out.println("\nEl numero ingresado por teclado es: " + numero );
     }
 }
