25f063b2ee2f747af0963755198683b19e877adc
lautaro valle lopez <lautarovallelopez@gmail.com>
2024-02-20 20:26:13 -0300

    use de condicional if

---
+++ src/Ejercicio7.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/*
+* Codifique un programa en Java que permita saber si un número ingresado
+por teclado es par o impar.
+* */
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numeroIngresado;
+        System.out.print("ingrese numero: ");
+        numeroIngresado = Integer.parseInt(scanner.nextLine());
+        if(numeroIngresado % 2 == 0) {
+            System.out.println("El numero es par");
+        } else {
+            System.out.println("El numero es impar");
+        }
+    }
+}
